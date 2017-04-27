a6a5c3042b6c5060ab8a6e6256e24339f2880bbf
Shunsuke Hayashi <hayashi@chieru.co.jp>
2017-04-27 10:55:19 +0900

    aタグパス修正

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -10,30 +10,12 @@ public class HelloController {
 		return
 				"<html>" +
 				"<head>" +
-				"<title>Index page</title>" +
-				"</head>" +
-				"<body>" +
-				"<h1>Index page!</h1>" +
-				"<div style=\"width: 300\">" +
-				"<span style=\"width: 130; float: left; color: #CCCCCC\">&lt;&lt;</span>" +
-				"<span style=\"width: 10;\">|</span>" +
-				"<a href=\"/chieru\" style=\"width: 130; float: right;\">CHIeru page&nbsp;&gt;&gt;</a>" +
-				"</div>" +
-				"</body>" +
-				"</html>";
-	}
-
-	@RequestMapping("/chieru")
-	public String chieru() {
-		return
-				"<html>" +
-				"<head>" +
 				"<title>CHIeru page</title>" +
 				"</head>" +
 				"<body>" +
 				"<h1>CHIeru page!</h1>" +
 				"<div style=\"width: 300\">" +
-				"<a href=\"/\" style=\"width: 130; float: left;\">&lt;&lt;&nbsp;Index page</a>" +
+				"<span style=\"width: 130; float: left; color: #CCCCCC\">&lt;&lt;</span>" +
 				"<span style=\"width: 10;\">|</span>" +
 				"<a href=\"/chieru/demo\" style=\"width: 130; float: right;\">Demo page&nbsp;&gt;&gt;</a>" +
 				"</div>" +
@@ -41,8 +23,8 @@ public class HelloController {
 				"</html>";
 	}
 
-	@RequestMapping("/chieru/demo")
-	public String demo() {
+	@RequestMapping("/demo")
+	public String chieru() {
 		return
 				"<html>" +
 				"<head>" +
@@ -51,7 +33,25 @@ public class HelloController {
 				"<body>" +
 				"<h1>Demo page!</h1>" +
 				"<div style=\"width: 300\">" +
-				"<a href=\"/chieru\" style=\"width: 130; float: left;\">&lt;&lt;CHIeru page&nbsp;</a>" +
+				"<a href=\"/chieru\" style=\"width: 130; float: left;\">&lt;&lt;&nbsp;CHIeru page</a>" +
+				"<span style=\"width: 10;\">|</span>" +
+				"<a href=\"/chieru/demo/test\" style=\"width: 130; float: right;\">Test page&nbsp;&gt;&gt;</a>" +
+				"</div>" +
+				"</body>" +
+				"</html>";
+	}
+
+	@RequestMapping("/demo/test")
+	public String demo() {
+		return
+				"<html>" +
+				"<head>" +
+				"<title>Test page</title>" +
+				"</head>" +
+				"<body>" +
+				"<h1>Test page!</h1>" +
+				"<div style=\"width: 300\">" +
+				"<a href=\"/chieru/demo\" style=\"width: 130; float: left;\">&lt;&lt;Demo page&nbsp;</a>" +
 				"<span style=\"width: 10;\">|</span>" +
 				"<span style=\"width: 130; float: right; color: #CCCCCC\">&gt;&gt;</span>" +
 				"</div>" +
